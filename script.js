22d5c669fd525e16fd21ccca29f41ec8b8179e64
emo-bean <emogene@pdx.edu>
2022-11-21 13:33:18 -0800

    added slick-emo

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function () {
+    // Your Code from here on down. Don't delete that line above!
+  
+    // code for single slider carousel
+    $('.single-slide').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      arrows: true,
+      autoplaySpeed: 3500,
+      infinite: true,
+      dots: true,
+      draggable: false
+    })
+  
+    // End of Your Code . Don't delete that line below!!
+  })
