7dd731cb46b29bac2ab433c74aca8b875229a54c
alannarisse <angrypirate@gmail.com>
2022-11-23 13:54:49 -0800

    slideshow stuff

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 $(function () {
-    // Your Code from here on down. Don't delete that line above!
-  
-    // code for single slider carousel
-    $('.single-slide').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      arrows: true,
-      autoplaySpeed: 3500,
-      infinite: true,
-      dots: true,
-      draggable: false
-    })
-  
-    // End of Your Code . Don't delete that line below!!
+  // Your Code from here on down. Don't delete that line above!
+
+  // code for single slider carousel
+  $('.single-slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 3500,
+    infinite: true,
+    dots: true,
+    draggable: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   })
+
+  // End of Your Code . Don't delete that line below!!
+})
